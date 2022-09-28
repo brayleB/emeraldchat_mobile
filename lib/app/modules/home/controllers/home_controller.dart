dcf4144bec9ce6e71e56af0007337a77366ff699
DESKTOP-NDECTHM\darkc <braylebautista22@gmail.com>
2022-09-28 15:05:05 +0800

    test socket

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -14,28 +14,28 @@ class HomeController extends GetxController {
 
   final count = 0.obs;
   @override
-  void onInit() async { 
-    print('sampple');
+  void onInit() async {   
+
+  checkIfLoggedIn();
   final prefs = await SharedPreferences.getInstance();
-  final String? cookie = prefs.getString('cookie'); 
-  print(cookie)  ;
+  final String? cookie = prefs.getString('cookie');   
   final channel = IOWebSocketChannel.connect(
     Uri.parse('wss://emeraldchat.com/cable'),
     pingInterval: Duration(seconds: 5),
      headers: {             
         'Host': 'emeraldchat.com',
         'Connection': 'Upgrade',
-        // 'Pragma': 'no-cache',
-        // 'Cache-Control': 'no-cache',
-        // 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
         'Upgrade': 'websocket',
-        // 'Origin': 'https://emeraldchat.com',
-        // 'Sec-WebSocket-Version': '13',
-        // 'Accept-Encoding': 'gzip, deflate, br',
-        // 'Accept-Language': 'en-US,en;q=0.9',
-        'Cookie': cookie.toString,
+        'Origin': 'https://emeraldchat.com',
+        'Sec-WebSocket-Version': '13',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Cookie': ' _ga=GA1.2.532322096.1663817054; __auc=05feef1e183633a688b468d4fc9; __stripe_mid=0aa1aa29-817f-4506-b045-d42ed9256e986bd729; _gid=GA1.2.353753828.1664261036; __asc=df2fd6f61837db05ecaad5cc770; __stripe_sid=e98eaba7-4bd2-4a24-b2f6-0e11e65b742bcbc941; user_id=MjU2Njg4OTc%3D--16fc806aa28d107b71038113a1754d1246801bb3; remember_token=rDGxVlZusF0VJLFb8xJ0ww; _prototype_app_session=MndORHQvY1EyZklwanZiYzhCWWFUVDNrSDRnVklrZVZwYm9yZmRZa2xZN3VybGlzcXR5VmpFaHVueEloTUdLa21nZXZBQWRFd21IeVY2cE9xbkwzaWhOMy9LaEpXK1RWQ1YzTkFUSHZOemNNMHBDRERRTWJicTNEc0hVZytWakdtUE5sb3BiaXhqTFY0QVNURStWcFFmbmQvemszVGVCZE90ZVhqdEtNSnhGYTVBa2dJLzZxU0ZkSEdnQjhDQkVieXFtVmtmWGI0SVNtWDlmZXJxYkFyTExDdE9GQXJpRmRHQ245RnEzbTA0bz0tLUJ6d3FOWG5wQXowS1c4U0Y2aEZGNEE9PQ%3D%3D--94b1953b33a89ed32b3a08eb86f182922dc62535; _gat=1',
         // 'Sec-WebSocket-Key': 'LbJ/gBj+M0XA+m64aZwL3g==',
-        // 'Sec-WebSocket-Extensions': 'permessage-deflate; client_max_window_bits',
+        'Sec-WebSocket-Extensions': 'permessage-deflate; client_max_window_bits',
         // 'Sec-WebSocket-Protocol': 'actioncable-v1-json, actioncable-unsupported '                             
         }, 
     );    
@@ -50,7 +50,7 @@ class HomeController extends GetxController {
           print('ws error $error');
         },
       );  
-    // checkIfLoggedIn();
+
     super.onInit();
   }
 
@@ -88,7 +88,7 @@ class HomeController extends GetxController {
             await pref.setBool('master', respCurrentUser.body['master'] ?? false);
             await pref.setBool('mod', respCurrentUser.body['mod'] ?? false);
             await pref.setString('username', respCurrentUser.body['username'] ?? '');
-            await pref.setBool('gold', respCurrentUser.body['gold'] ?? false);              
+            await pref.setBool('gold', respCurrentUser.body['gold'] ?? false);                          
             Get.to(()=>DashboardView());          
           }  
         }  
